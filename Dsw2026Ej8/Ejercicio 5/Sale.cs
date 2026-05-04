751efd14970e619f2bb2af17472794f1f86982f9
Gabriel Azubel <gabrielazubel63t@gmail.com>
2026-05-04 15:52:45 -0300

    Realizo ejercicio 5

---
+++ Dsw2026Ej8/Ejercicio 5/Sale.cs
@@ -0,0 +1,28 @@
+﻿namespace Dsw2026Ej8;
+
+public class Sale
+{
+    public decimal Subtotal;
+
+    public virtual decimal CalculateTotal()
+    {
+        return Subtotal;
+    }
+}
+
+public class RetailSale : Sale
+{
+    public override decimal CalculateTotal()
+    {
+        return Subtotal;
+    }
+}
+
+public class WholesaleSale : Sale
+{
+    public override decimal CalculateTotal()
+    {
+        return Subtotal * 0.90m;
+    }
+}
+
--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            // Ejercicio 1
+            // -------------------------------------------------------
+            // ------------------   Ejercicio 1   --------------------
+            // -------------------------------------------------------
             /*  
             ProductHelper helper = new ProductHelper();
 
@@ -12,8 +14,9 @@
 
             Console.WriteLine(etiqueta);
             */
-
-            // Ejercicio 2
+            // -------------------------------------------------------
+            // ------------------   Ejercicio 2   --------------------
+            // -------------------------------------------------------
             /*
             Problema2 problema2 = new Problema2();
 
@@ -24,8 +27,9 @@
             string resultado2 = problema2.CrearResumenVenta(1234, "Mouse Inalámbrico", -1, 15000.50m);
             Console.WriteLine(resultado2);
             */
-
-            // Ejercicio 3
+            // -------------------------------------------------------
+            // ------------------   Ejercicio 3   --------------------
+            // -------------------------------------------------------
             /*
             Product producto = new Product();
             producto.Description = "Mouse Original";
@@ -37,9 +41,10 @@
 
             Console.WriteLine(resultado);
             */
-
-            // Ejercicio 4
-
+            // -------------------------------------------------------
+            // ------------------   Ejercicio 4   --------------------
+            // -------------------------------------------------------
+            /*
             Problema4 problema = new Problema4();
 
             Console.WriteLine(problema.CalcularPromedio(10, 8, 6));
@@ -47,6 +52,21 @@
             Console.WriteLine(problema.CalcularPromedio(null, -10, 7));
 
             Console.WriteLine(problema.CalcularPromedio(null, null, null));
+            */
+            // -------------------------------------------------------
+            // ------------------   Ejercicio 5   --------------------
+            // -------------------------------------------------------
+            Problema5 problema = new Problema5();
+
+            RetailSale ventaMenor = new RetailSale();
+            ventaMenor.Subtotal = 1000m;
+            decimal totalMenor = problema.ObtenerImporteFinal(ventaMenor);
+            Console.WriteLine("Total minorista: " + totalMenor);
+
+            WholesaleSale ventaMayor = new WholesaleSale();
+            ventaMayor.Subtotal = 1000m;
+            decimal totalMayor = problema.ObtenerImporteFinal(ventaMayor);
+            Console.WriteLine("Total mayorista: " + totalMayor);
         }
     }
 }
